89a1dad6484d85f15fab2a482d74dbd19ef5e1f5
lokeshlavate56-abcd <lokeshlavate56@gmail.com>
2025-11-08 15:57:34 +0530

    Add files via upload

---
+++ exp-1A.c
@@ -0,0 +1,8 @@
+// Lokesh Lavate,UIN:-251P083, YEAR-F.E,DIV.D
+#include <stdio.h>
+
+int main() {
+    printf("Name:- Lokesh Lavate, \nage:-18, \nclass:-F.E, \nDIV:-D, \nUIN:-251P083\n");
+
+    return 0;
+}
---
+++ exp-1b.c
@@ -0,0 +1,21 @@
+// Lokesh Lavate,UIN:-251P083,YEAR:-F.E,DIV.D
+#include <stdio.h>
+
+int main() {
+    float phy, chem, math, avg;
+    printf("Enter Physics marks: ");
+    scanf("%f", &phy);
+
+    printf("Enter Chemistry marks: ");
+    scanf("%f", &chem);
+
+    printf("Enter Mathematics marks: ");
+    scanf("%f", &math);
+    avg = (phy + chem + math) / 3;
+    printf("\nAverage marks in PCM = %.2f\n", avg);
+    (avg >= 50) 
+        ? printf("Student is Eligible for Admission.\n")
+        : printf("Student is NOT Eligible for Admission.\n");
+
+    return 0;
+}
